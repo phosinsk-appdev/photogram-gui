13eb457829a1ff7d4bf0eec7118583e8049dc4b5
Patrick Hosinski <phosinsk@chicagobooth.edu>
2023-05-02 02:57:00 +0000

    pausing

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def index
+    
+    matching_users = User.all
+
+    @list_of_users = matching_users.order({ :username => :asc})
+
+    render({:template => "user_templates/index.html.erb"})
+
+  end
+
+  def show_details
+
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({:username => url_username})
+    @the_user = matching_usernames.at(0)
+
+    if @the_user == nil
+      redirect_to("/404")
+    else
+      render({:template => "user_templates/show.html.erb"})
+    end
+
+  end
+
+end
